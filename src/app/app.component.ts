4062cb2e3360664f302c48d5a37fc5139cca860a
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-09 17:32:55 -0500

    Se ha creado e implementado un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Emiro1Component } from "./emiro-1/emiro-1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Emiro1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hola mundo desde Angular';
 }
---
+++ src/app/emiro-1/emiro-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emiro-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './emiro-1.component.html',
+  styleUrl: './emiro-1.component.css'
+})
+export class Emiro1Component {
+  tituloNuevo = "Nuevo titulo Emiro1"
+}
