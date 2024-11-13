7962189d9773aef978da745a5e25c24f7f196cc6
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-13 18:36:54 -0500

    Tenemos el codigo de la clase modelo

---
+++ src/app/a2-tienda/a2-tienda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a2-tienda',
+  standalone: true,
+  imports: [],
+  templateUrl: './a2-tienda.component.html',
+  styleUrl: './a2-tienda.component.css'
+})
+export class A2TiendaComponent {
+
+}
